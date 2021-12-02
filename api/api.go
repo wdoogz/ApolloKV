21f585403950a294734d3698ba0cf077f47563c8
Wesley <wed30@pitt.edu>
2021-12-01 22:34:58 -0500

    adding post functionality and mem store

--- api/api.go
+++ api/api.go
@@ -1,25 +1,44 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var Store map[string]interface{}
+
 // Api function
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-	fmt.Println(r.Method)
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal("error body not readable")
+	fmt.Println("HomePage")
+}
+
+func writeKV(w http.ResponseWriter, r *http.Request) {
+	//var data string
+	//var store map[string]interface{}
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal("error body not readable")
+		}
+		//data = string(body)
+		json.Unmarshal([]byte(body), &Store)
 	}
-	fmt.Println(string(body))
+}
+
+func kvStore(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		for k, v := range Store {
+			fmt.Fprintf(w, "%s -> %s \n", k, v)
+		}
+	}
 }
 
 func HandleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/kv", kvStore)
+	http.HandleFunc("/write", writeKV)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
